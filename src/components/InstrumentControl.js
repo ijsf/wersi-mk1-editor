96ee82a896922a6a1ddeea76ac3d0177912a343e
Cecill Etheredge <code@ijsf.nl>
2016-10-18 03:47:51 +0200

    Import support.

--- src/components/InstrumentControl.js
+++ src/components/InstrumentControl.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Panel, Button, ButtonGroup, ButtonToolbar, Checkbox, Modal, Col, Row, Form, FormGroup, InputGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import Dropzone from 'react-dropzone';
 
 import reactMixin from 'react-mixin';
 import reactor from 'modules/flux';
@@ -23,6 +24,8 @@ class InstrumentControl extends Component {
   }
   
   _handleImport() {
+    // Show import modal
+    this.setState({ import: "Import your instrument below." });
   }
   
   _handleExport() {
@@ -40,7 +43,7 @@ class InstrumentControl extends Component {
       freq: freq
     };
 
-    // Store to state
+    // Store to state, show export modal
     let file = new Blob([JSON.stringify(json)], { type: "application/json" });
     let url = URL.createObjectURL(file);
     this.setState({ export: url });
@@ -98,14 +101,86 @@ class InstrumentControl extends Component {
         <Modal.Body>
           <p>
             {this.state.export ? "Your instrument has been exported to a JSON file." : null}
+            {this.state.import ? this.state.import : null}
           </p>
-          <p>
-            {this.state.export ?
-              (<a download={icb.get('name') + '.json'} href={this.state.export} onClick={() => this.setState({ export: null })}>
+          {this.state.export
+            ? (
+              <p>
+                <a download={icb.get('name') + '.json'} href={this.state.export} onClick={() => this.setState({ export: null })}>
                 Click here to download.
-              </a>) : null
-            }
-          </p>
+                </a>
+              </p>
+            )
+            : (
+              <Dropzone style={{ width: '100%', border: '2px dashed #ccc', padding: 10, cursor: 'pointer' }}
+                onDrop={(files) => {
+                  if (files && files.length == 1) {
+                    let fr = new FileReader();
+                    fr.onload = (e) => {
+                      try {
+                        const json = JSON.parse(e.target.result);
+                        const icb = json.icb;
+                        
+                        // Retrieve ICB addresses
+                        let vcfAddress = icb.vcfAddress;
+                        let waveAddress = icb.waveAddress;
+                        let amplAddress = icb.amplAddress;
+                        let freqAddress = icb.freqAddress;
+                        console.log("Original ICB addresses: vcf " + vcfAddress + " wave " + waveAddress + " ampl " + amplAddress + " freq " + freqAddress);
+                        
+                        // Override these with 1-to-1 Wersi mapping
+                        vcfAddress = this.props.instrumentAddress - 1;
+                        waveAddress = this.props.instrumentAddress - 1;
+                        amplAddress = this.props.instrumentAddress - 1;
+                        freqAddress = this.props.instrumentAddress - 1;
+                        console.log("Remapped ICB addresses: vcf " + vcfAddress + " wave " + waveAddress + " ampl " + amplAddress + " freq " + freqAddress);
+
+                        // Send to SysEx
+                        this.props.client.setICB(this.props.instrumentAddress, toImmutable(json.icb))
+                        //.then(() => this.props.client.getICB(this.props.instrumentAddress))
+                        //.then((data) => instrumentActions.update(this.props.instrumentAddress, 'icb', toImmutable(data)))
+                        .then(() => instrumentActions.update(this.props.instrumentAddress, 'icb', toImmutable(json.icb)))
+
+                        .then((data) => this.props.client.setVCF(vcfAddress, toImmutable(json.vcf)))
+                        //.then(() => this.props.client.getVCF(vcfAddress))
+                        //.then((data) => instrumentActions.update(vcfAddress, 'vcf', toImmutable(data)))
+                        .then(() => instrumentActions.update(vcfAddress, 'vcf', toImmutable(json.vcf)))
+
+                        .then((data) => this.props.client.setFixWave(waveAddress, json.wave))
+                        //.then(() => this.props.client.getFixWave(waveAddress))
+                        //.then((data) => instrumentActions.update(waveAddress, 'wave', toImmutable(data)))
+                        .then(() => instrumentActions.update(waveAddress, 'wave', toImmutable(json.wave)))
+
+                        .then((data) => this.props.client.setAmpl(amplAddress, toImmutable(json.ampl)))
+                        //.then(() => this.props.client.getAmpl(amplAddress))
+                        //.then((data) => instrumentActions.update(amplAddress, 'ampl', toImmutable(data)))
+                        .then(() => instrumentActions.update(amplAddress, 'ampl', toImmutable(json.ampl)))
+                        
+//                          .then((data) => this.props.client.setFreq(freqAddress, toImmutable(json.freq)))
+//                          .then(() => this.props.client.getFreq(freqAddress))
+//                          .then((data) => instrumentActions.update(freqAddress, 'freq', toImmutable(data)))
+
+                        .then(() => this.props.client.reloadInstrument(this.props.instrumentAddress))
+                        ;
+                        
+                        // Hide modal and reset state variables
+                        this.setState({ import: null, name: null });
+                      }
+                      catch (e) {
+                        this.setState({ import: "Could not load your JSON file! Please try again." });
+                        console.error(e);
+                      }
+                    };
+                    fr.readAsText(files[0]);
+                  }
+                }
+              }>
+                <p style={{ margin: 0, textAlign: 'center' }}>
+                  Please drop your instrument JSON file here, or click here to upload your file.
+                </p>
+              </Dropzone>
+            )
+          }
         </Modal.Body>
         <Modal.Footer>
           <Button bsStyle="primary" onClick={() => this.setState({ export: null, import: null })}>Cancel</Button>

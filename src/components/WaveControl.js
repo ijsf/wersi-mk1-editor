49f6bce262c955c90e758a3c6055f90ba5edcfb4
Cecill Etheredge <code@ijsf.nl>
2016-10-22 14:58:48 +0200

    Loading indicator for Wave.

--- src/components/WaveControl.js
+++ src/components/WaveControl.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Panel, Button, Checkbox, Modal, Col, Form, FormGroup, InputGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import Loader from 'react-loader-advanced';
 
 import reactor from 'modules/flux';
 import { toImmutable } from 'nuclear-js';
@@ -13,6 +14,7 @@ export default class WaveControl extends Component {
     super();
     
     this.state = {
+      loading: false
     };
   }
   
@@ -59,19 +61,22 @@ export default class WaveControl extends Component {
   
   _handleSave() {
     // Send to SysEx
-    this.props.client.setFixWave(this.props.waveAddress, this.state.wave.toJS())
-    .then(() => {
-      // Refresh data in case the Wersi has made any changes
-      return this.props.client.getFixWave(this.props.waveAddress);
-    })
-    .then((data) => {
-      // Update store
-      instrumentActions.update(this.props.waveAddress, 'wave', toImmutable(data));
+    this.setState({ loading: true }, () => {
+      this.props.client.setFixWave(this.props.waveAddress, this.state.wave.toJS())
+      .then(() => {
+        // Refresh data in case the Wersi has made any changes
+        return this.props.client.getFixWave(this.props.waveAddress);
+      })
+      .then((data) => {
+        // Update store
+        instrumentActions.update(this.props.waveAddress, 'wave', toImmutable(data));
+        this.setState({ loading: false });
       
-      // Reload instrument
-      return this.props.client.reloadInstrument(this.props.firstInstrumentAddress);
-    })
-    ;
+        // Reload instrument
+        return this.props.client.reloadInstrument(this.props.firstInstrumentAddress);
+      })
+      ;
+    });
   }
 
   render() {
@@ -82,17 +87,19 @@ export default class WaveControl extends Component {
     );
     
     return (
-      <Panel header={header} collapsible defaultExpanded>
-        <div className="clearfix">
-          <Button onClick={this._handleSave.bind(this)} className="pull-right" bsStyle="primary">
-            Send
-          </Button>
-        </div>
-        <Wave client={this.props.client} waveSet='bassData' waveAddress={waveAddress} />
-        <Wave client={this.props.client} waveSet='tenorData' waveAddress={waveAddress} />
-        <Wave client={this.props.client} waveSet='altoData' waveAddress={waveAddress} />
-        <Wave client={this.props.client} waveSet='sopranoData' waveAddress={waveAddress} />
-      </Panel>
+      <Loader show={this.state.loading} message={(<h5>« Downloading... »</h5>)} contentBlur={2}>
+        <Panel header={header} collapsible defaultExpanded>
+          <div className="clearfix">
+            <Button onClick={this._handleSave.bind(this)} className="pull-right" bsStyle="primary">
+              Send
+            </Button>
+          </div>
+          <Wave client={this.props.client} waveSet='bassData' waveAddress={waveAddress} />
+          <Wave client={this.props.client} waveSet='tenorData' waveAddress={waveAddress} />
+          <Wave client={this.props.client} waveSet='altoData' waveAddress={waveAddress} />
+          <Wave client={this.props.client} waveSet='sopranoData' waveAddress={waveAddress} />
+        </Panel>
+      </Loader>
     );
   }
 }

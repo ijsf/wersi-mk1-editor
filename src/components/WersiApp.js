7457f99ddc34dcdcb18e7619cda3e6f09cb96321
Cecill Etheredge <code@ijsf.nl>
2017-01-04 17:43:02 +0100

    Added disclaimer.

--- src/components/WersiApp.js
+++ src/components/WersiApp.js
@@ -34,10 +34,16 @@ export default class WersiApp extends Component {
     
     return (
       <div>
-        <PageHeader>
-          Wersi MK1 editor
-          <sub>{this.props.version}</sub>
-        </PageHeader>
+        <div className="page-header">
+          <h1 style={{float: 'left'}}>
+            MK1 editor
+            <sub>{this.props.version}</sub>
+          </h1>
+          <h1 style={{float: 'right'}}>
+            wer.si
+          </h1>
+          <hr style={{border: 0, clear: 'both'}}/>
+        </div>
         {midiConfig}
         <Instrument
           instrumentAddress={WersiClient.ADDRESS.CV(0)}
@@ -45,7 +51,15 @@ export default class WersiApp extends Component {
           ref={(ref) => {
             this.instrument = ref;
           }}
-          />
+        />
+        <footer className="page-footer">
+          <p>
+            This software is no way affiliated with, authorized, maintained, sponsored or endorsed with Wersi nor any of its affiliates or subsidiaries.
+          </p>
+          <p>
+            Available as open source software under the <a href="https://raw.githubusercontent.com/ijsf/wersi-mk1-editor/master/LICENSE">AGPLv3</a> license @ <a href="http://wer.si">wer.si</a>.
+          </p>
+        </footer>
       </div>
     );
   }

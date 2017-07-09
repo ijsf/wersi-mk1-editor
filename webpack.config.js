c327447479596a76d821aece21a63dbc480b0dff
Cecill Etheredge <code@ijsf.nl>
2017-07-09 23:53:43 +0200

    Added babel-polyfill.

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
+    'babel-polyfill',
     './src/index'
   ],
   output: {
